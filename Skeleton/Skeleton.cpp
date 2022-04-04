d9b0e955b580bc5301a50f9a509616d5637e3e5b
viktoriaseregelyes <87902410+viktoriaseregelyes@users.noreply.github.com>
2022-04-04 13:43:20 +0200

    legyen már vonal

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -64,107 +64,76 @@ unsigned int vao, vbo;
 float p = 0, q = 0;
 
 class Atom {
-public: float charge = 1.6;
-		int quantity = 1;
-		GLfloat x, y, z;
-		std::vector<vec4> points;
+public: float charge = (1,6 * rand()) % 5;
+		int quantity = (1 * rand()) % 100;
+		float x, y, z;
 		const int nTesselatedVertices = 50;
+		std::vector<vec4> points;
 
 	Atom() {
-		for (int i = 0; i < nTesselatedVertices; i++) {
-			GLfloat phi = i * 2.0f * M_PI / 50;
-			points.push_back(vec4((cosf(phi) + x) / quantity, (sinf(phi) + y) / quantity, z / quantity, 1));
-		}
-
-		charge *= rand() % 5;
-		quantity *= rand() % 100;
 		x = 0.0f; y = 0.0f; z = 0.0f;
 	}
 
 	Atom(GLfloat x1, GLfloat y1, GLfloat z1) {
-		for (int i = 0; i < nTesselatedVertices; i++) {
-			GLfloat phi = i * 2.0f * M_PI / 50;
-			points.push_back(vec4((cosf(phi) + x) / quantity, (sinf(phi) + y) / quantity, z / quantity, 1));
-		}
-
-		charge *= rand() % 5;
-		quantity *= rand() % 100;
 		x = x1; y = y1; z = z1;
 	}
 
 public:
-//	vec4 getCoord() { return vec4(x,y,z); }
-	GLfloat getX() { return x; }
-	GLfloat getY() { return y; }
-	GLfloat getZ() { return z; }
+	float getX() { return x; }
+	float getY() { return y; }
+	float getZ() { return z; }
 
 	void create() {
+		for (int i = 0; i < nTesselatedVertices; i++) {
+			float phi = i * 2.0f * M_PI / 50;
+			points.push_back(vec4((cosf(phi) + x) / quantity, (sinf(phi) + y) / quantity, z / quantity, 1));
+		}
+		int location = glGetUniformLocation(gpuProgram.getId(), "color");
+		glUniform3f(location, 0.0f, 0.0f, 1 / charge);
+
 		glGenVertexArrays(1, &vao);
 		glBindVertexArray(vao);
-		unsigned int vbo[2];
-		glGenBuffers(2, &vbo[0]);
-		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
-		float colors[] = { 0, 0, 1 * charge };
-		glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(colors), &colors[0], GL_STATIC_DRAW);
-
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-		glBufferData(GL_ARRAY_BUFFER, nTesselatedVertices * sizeof(vec4), &points[0], GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), NULL);
 	}
 
 	void drawAtom() {
-		glBindVertexArray(vao);
+		glBufferData(GL_ARRAY_BUFFER, nTesselatedVertices * sizeof(vec4), &points[0], GL_STATIC_DRAW);
 		glDrawArrays(GL_TRIANGLE_FAN, 0, nTesselatedVertices);
 	}
 };
 
 class Line {
-public: std::vector<vec2> points;
+public: std::vector<vec4> points;
 	    std::vector<float> data;
 
 	  Line(Atom a, Atom b) {
-		  points.push_back(vec2(a.getX(), a.getY()));
-		  points.push_back(vec2(b.getX(), b.getY()));
-		  data.push_back(a.getX());
-		  data.push_back(a.getY());
-		  data.push_back(b.getX());
-		  data.push_back(b.getY());
+		  points.push_back(vec4(a.getX(), a.getY(), a.getZ(), 1));
+		  points.push_back(vec4(b.getX(), b.getY(), a.getZ(), 1));
 	  }
 
 public:
 	void create() {
 		glGenVertexArrays(1, &vao);
 		glBindVertexArray(vao);
-		glGenBuffers(2, &vbo);
+		glGenBuffers(1, &vbo);
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(0));
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
-		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_DYNAMIC_DRAW);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(float), NULL);
+		glBufferData(GL_ARRAY_BUFFER, 2 * sizeof(vec4), &points[0], GL_STATIC_DRAW);
 	}
 	
 	void drawLine() {
 		glBindVertexArray(vao);
-		glDrawArrays(GL_LINE_STRIP, 0, data.size() / 5);
+		glDrawArrays(GL_LINE_STRIP, 0, points.size());
 	}
 };
-
-Atom a(5.0f, 5.0f, 0.0f);
-Atom b(3.0f, 3.0f, 0.0f);
-Line l(a, b);
 
 void onInitialization() {
 	glViewport(0, 0, 600, 600);
 	glutInitWindowSize(600, 600);
-	
-	a.create();
-	b.create();
-	l.create();
 
 	gpuProgram.create(vertexSource, fragmentSource, "outColor");
 }
@@ -174,7 +143,7 @@ void onDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	int location = glGetUniformLocation(gpuProgram.getId(), "color");
-	glUniform3f(location, 1.0f, 1.0f, 0.0f);
+	//glUniform3f(location, 1.0f, 1.0f, 1.0f);
 
 	float MVPtransf[4][4] = { 1, 0, 0, 0,
 							  0, 1, 0, 0,
@@ -183,12 +152,19 @@ void onDisplay() {
 
 	location = glGetUniformLocation(gpuProgram.getId(), "MVP");
 	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);
-
-	glBindVertexArray(vao);  // Draw call
+	
+	glBindVertexArray(vao);
+	Atom a(5.0f, 5.0f, 0.0f);
+	a.create();
 	a.drawAtom();
+	Atom b(10.0f, 10.0f, 0.0f);
+	b.create();
 	b.drawAtom();
+	Line l(a, b);
+	l.create();
 	l.drawLine();
-	glutSwapBuffers(); // exchange buffers for double buffering
+	glutSwapBuffers();
+	
 }
 
 void onKeyboard(unsigned char key, int pX, int pY) {
@@ -205,16 +181,14 @@ void onKeyboard(unsigned char key, int pX, int pY) {
 void onKeyboardUp(unsigned char key, int pX, int pY) {
 }
 
-void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
+void onMouseMotion(int pX, int pY) {
+	float cX = 2.0f * pX / windowWidth - 1;
 	float cY = 1.0f - 2.0f * pY / windowHeight;
 	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
 }
 
-void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
+void onMouse(int button, int state, int pX, int pY) {
+	float cX = 2.0f * pX / windowWidth - 1;
 	float cY = 1.0f - 2.0f * pY / windowHeight;
 
 	char * buttonStat;
@@ -231,5 +205,5 @@ void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel co
 }
 
 void onIdle() {
-	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+	long time = glutGet(GLUT_ELAPSED_TIME);
 }
